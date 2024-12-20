05e0919c57922158024966011b2717d4ff7f81f4
dharshan-kumarj <jdharshankumar18@gmail.com>
2024-12-20 12:18:02 +0530

    implemeted the left section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './fonts/fonts.css'
 import insta from './assets/images/insta.svg'
 import discord from './assets/images/discord.svg'
 import link from './assets/images/link.svg'
@@ -12,13 +13,13 @@ function App() {
     <footer className="bg-red-500 min-h-screen">
       <div className="mx-auto w-full max-w-screen-xl p-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div className="flex flex-col space-y-4">
+          <div className="flex flex-col pt-16 space-y-4">
             <div>
-              <h1 className="text-2xl p-10 font-bold text-white">[ -_ ] HACKCELERATE</h1>
-              <p className="text-white font-semibold text-sm mt-2">INDIA'S BIGGEST COMMUNITY HACKATHON</p>
-              <p className="text-white text-sm mt-2">March 8th-9th, 2025 - Chennai</p>
+              <h1 className=" pr-4 font-bold font-bricolage text-white"><span className='text-3xl italic'>[ -_ ]</span> <span className='text-3xl'>HACKCELERATE</span></h1>
+              <p className="text-white pr-4 pt-5 font-semibold font-instrument text-sm ">INDIA'S BIGGEST COMMUNITY HACKATHON</p>
+              <p className="text-white pt-3 font-bricolage text-base pr-14">March 8th-9th, 2025 - Chennai</p>
             </div>
-            <div className="flex space-x-4 mt-4">
+            <div className="flex px-11 pt-1 space-x-7 mt-4">
               <img src={insta} alt="Instagram" className="w-8 h-8" />
               <img src={link} alt="LinkedIn" className="w-8 h-8" />
               <img src={discord} alt="Discord" className="w-8 h-8" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bricolage': ['"Bricolage Grotesque"', 'sans-serif'],
+        'instrument': ['"Instrument Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
